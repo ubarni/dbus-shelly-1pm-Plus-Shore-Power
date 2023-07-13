feb703ff146cd769ad3dde5b52f06658ea6c73a2
Uwe Barnstein <uwe@barnstein.de>
2023-07-13 16:06:26 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
 rm /service/$SERVICE_NAME
-kill $(pgrep -f 'supervise dbus-shelly-1pm-pvinverter')
+kill $(pgrep -f 'supervise dbus-shelly-1pm-plus-shore-power')
 chmod a-x $SCRIPT_DIR/service/run
 $SCRIPT_DIR/restart.sh
