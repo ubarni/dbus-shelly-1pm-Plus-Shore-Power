203150f47d2596edde7f65cda7bc9ecd9bcf070f
Uwe Barnstein <uwe@barnstein.de>
2024-11-05 11:40:41 +0100

    Update dbus-shelly-1pm-plus-shore-power.py

--- dbus-shelly-1pm-plus-shore-power.py
+++ dbus-shelly-1pm-plus-shore-power.py
@@ -23,7 +23,7 @@ from vedbus import VeDbusService
 
 
 class DbusShelly1pmService:
-    def __init__(self,servicename,paths,productname="Shelly(Plus) 1PM",connection="Shelly(Plus) 1PM HTTP JSON service",):
+    def __init__(self,paths,productname="Shelly(Plus) 1PM",connection="Shelly(Plus) 1PM HTTP JSON service",):
         config = self._getConfig()
         deviceinstance = int(config["DEFAULT"]["Deviceinstance"])
         customname = config["DEFAULT"]["CustomName"]
